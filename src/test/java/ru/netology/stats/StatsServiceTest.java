68ce9a1e2f6fd6455a003fe5a71eec64103c23c7
Anastaia <kvesteriko88@yandex.ru>
2023-06-11 12:35:57 +0300

    fixed bug in test 2

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -19,8 +19,8 @@ class StatsServiceTest {
     public void testAverage() {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long actual = service.sum(sales);
-        long expected = 180 / 12;
+        long actual = service.sum(sales)/12;
+        long expected = 180/12;
         Assertions.assertEquals(expected, actual);
     }
 
